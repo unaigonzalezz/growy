3b765600b6f3988f9e5c1318ac709024f4d73f30
Unai González <unaiitxuu@gmail.com>
2024-06-06 09:34:59 +0200

    Dark Mode on all browsers

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="dark:bg-gray-900"><NextTopLoader />{children}</body>
+      <body className="dark bg-gray-900"><NextTopLoader />{children}</body>
     </html>
   );
 }
